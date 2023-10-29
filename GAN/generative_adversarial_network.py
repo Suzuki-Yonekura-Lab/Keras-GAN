c326f1dc173c2d51210be86cb21fbee2e08624fd
geeeeorge <yuto.george@i.softbank.jp>
2023-10-30 07:13:25 +0900

    feat: GANの実装

--- GAN/generative_adversarial_network.py
+++ GAN/generative_adversarial_network.py
@@ -1,4 +1,6 @@
+import tensorflow as tf
 from tensorflow import keras
+from tensorflow.keras import layers
 import matplotlib.pyplot as plt
 import os
 import gdown
@@ -7,6 +9,134 @@ from zipfile import ZipFile
 
 # ハイパーパラメータ
 BATCH_SIZE = 32
+LATENT_DIM = 128
+EPOCHS = 1
+
+# Discriminator 作成
+discriminator = keras.Sequential(
+    [
+        keras.Input(shape=(64, 64, 3)),
+        layers.Conv2D(64, kernel_size=4, strides=2, padding="same"),
+        layers.LeakyReLU(alpha=0.2),
+        layers.Conv2D(128, kernel_size=4, strides=2, padding="same"),
+        layers.LeakyReLU(alpha=0.2),
+        layers.Conv2D(128, kernel_size=4, strides=2, padding="same"),
+        layers.LeakyReLU(alpha=0.2),
+        layers.Flatten(),
+        layers.Dropout(0.2),
+        layers.Dense(1, activation="sigmoid"),
+    ],
+    name="discriminator",
+)
+discriminator.summary()
+
+# Generator 作成
+generator = keras.Sequential(
+    [
+        keras.Input(shape=(LATENT_DIM,)),
+        layers.Dense(8 * 8 * 128),
+        layers.Reshape((8, 8, 128)),
+        layers.Conv2DTranspose(128, kernel_size=4, strides=2, padding="same"),
+        layers.LeakyReLU(alpha=0.2),
+        layers.Conv2DTranspose(256, kernel_size=4, strides=2, padding="same"),
+        layers.LeakyReLU(alpha=0.2),
+        layers.Conv2DTranspose(512, kernel_size=4, strides=2, padding="same"),
+        layers.LeakyReLU(alpha=0.2),
+        layers.Conv2D(3, kernel_size=5, padding="same", activation="sigmoid"),
+    ],
+    name="generator",
+)
+generator.summary()
+
+
+# GAN
+class GAN(keras.Model):
+    def __init__(self, discriminator, generator, latent_dim):
+        super().__init__()
+        self.discriminator = discriminator
+        self.generator = generator
+        self.latent_dim = latent_dim
+
+    def compile(self, d_optimizer, g_optimizer, loss_fn):
+        super().compile()
+        self.d_optimizer = d_optimizer
+        self.g_optimizer = g_optimizer
+        self.loss_fn = loss_fn
+        self.d_loss_metric = keras.metrics.Mean(name="d_loss")
+        self.g_loss_metric = keras.metrics.Mean(name="g_loss")
+
+    @property
+    def metrics(self):
+        return [
+            self.d_loss_metric,
+            self.g_loss_metric,
+        ]
+
+    def train_step(self, real_images):
+        batch_size = tf.shape(real_images)[0]
+
+        # 潜在空間状の点をサンプリング
+        random_latent_vectors = tf.random.normal(shape=(batch_size, self.latent_dim))
+
+        # 偽画像作成
+        generated_images = self.generator(random_latent_vectors)
+
+        # 偽画像と真画像を一つに
+        combined_images = tf.concat([generated_images, real_images], axis=0)
+
+        # 真 -> 0, 偽 -> 1
+        labels = tf.concat(
+            [tf.ones((batch_size, 1)), tf.zeros((batch_size, 1))], axis=0
+        )
+
+        # ランダムノイズをラベルに付与
+        labels += 0.05 * tf.random.uniform(tf.shape(labels))
+
+        # train the discriminator
+        with tf.GradientTape() as tape:
+            predictions = self.discriminator(combined_images)
+            d_loss = self.loss_fn(labels, predictions)
+        grads = tape.gradient(d_loss, self.discriminator.trainable_weights)
+        self.d_optimizer.apply_gradients(
+            zip(grads, self.discriminator.trainable_weights)
+        )
+
+        # 潜在空間状の点をサンプリング
+        random_latent_vectors = tf.random.normal(shape=(batch_size, self.latent_dim))
+
+        # 全部真画像のラベルを用意
+        misleading_labels = tf.zeros((batch_size, 1))
+
+        # train the generator
+        with tf.GradientTape() as tape:
+            predictions = self.discriminator(self.generator(random_latent_vectors))
+            g_loss = self.loss_fn(misleading_labels, predictions)
+        grads = tape.gradient(g_loss, self.generator.trainable_weights)
+        self.g_optimizer.apply_gradients(zip(grads, self.generator.trainable_weights))
+
+        # update metrics
+        self.d_loss_metric.update_state(d_loss)
+        self.g_loss_metric.update_state(g_loss)
+        return {
+            "d_loss": self.d_loss_metric.result(),
+            "g_loss": self.g_loss_metric.result(),
+        }
+
+
+class GANMonitor(keras.callbacks.Callback):
+    def __init__(self, num_img=3, latent_dim=128):
+        self.num_img = num_img
+        self.latent_dim = latent_dim
+
+    def on_epoch_end(self, epoch, logs=None):
+        random_latent_vectors = tf.random.normal(shape=(self.num_img, self.latent_dim))
+        generated_images = self.model.generator(random_latent_vectors)
+        generated_images *= 255
+        generated_images.numpy()
+        for i in range(self.num_img):
+            img = keras.utils.array_to_img(generated_images[i])
+            img.save("generated_img_%03d_%d.png" % (epoch, i))
+
 
 if __name__ == "__main__":
     # データセット用意
@@ -33,3 +163,17 @@ if __name__ == "__main__":
         plt.axis("off")
         plt.imshow((x.numpy() * 255).astype("int32")[0])
         break
+
+    # train GAN
+    gan = GAN(discriminator=discriminator, generator=generator, latent_dim=LATENT_DIM)
+    gan.compile(
+        d_optimizer=keras.optimizers.Adam(learning_rate=0.0001),
+        g_optimizer=keras.optimizers.Adam(learning_rate=0.0001),
+        loss_fn=keras.losses.BinaryCrossentropy(),
+    )
+
+    gan.fit(
+        dataset,
+        epochs=EPOCHS,
+        callbacks=[GANMonitor(num_img=10, latent_dim=LATENT_DIM)],
+    )
