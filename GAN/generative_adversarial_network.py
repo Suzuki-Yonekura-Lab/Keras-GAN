70417b589660a7c3fb6f0218d98f61aaa2bdfb26
geeeeorge <yuto.george@i.softbank.jp>
2023-12-14 13:20:52 +0900

    feat: GANをmnistに対応

--- GAN/generative_adversarial_network.py
+++ GAN/generative_adversarial_network.py
@@ -10,12 +10,12 @@ from zipfile import ZipFile
 # ハイパーパラメータ
 BATCH_SIZE = 32
 LATENT_DIM = 128
-EPOCHS = 1
+EPOCHS = 50
 
 # Discriminator 作成
 discriminator = keras.Sequential(
     [
-        keras.Input(shape=(64, 64, 3)),
+        keras.Input(shape=(28, 28, 1)),
         layers.Conv2D(64, kernel_size=4, strides=2, padding="same"),
         layers.LeakyReLU(alpha=0.2),
         layers.Conv2D(128, kernel_size=4, strides=2, padding="same"),
@@ -34,15 +34,11 @@ discriminator.summary()
 generator = keras.Sequential(
     [
         keras.Input(shape=(LATENT_DIM,)),
-        layers.Dense(8 * 8 * 128),
-        layers.Reshape((8, 8, 128)),
-        layers.Conv2DTranspose(128, kernel_size=4, strides=2, padding="same"),
+        layers.Dense(7 * 7 * 128),
+        layers.Reshape((7, 7, 128)),
+        layers.Conv2DTranspose(64, kernel_size=4, strides=2, padding="same"),
         layers.LeakyReLU(alpha=0.2),
-        layers.Conv2DTranspose(256, kernel_size=4, strides=2, padding="same"),
-        layers.LeakyReLU(alpha=0.2),
-        layers.Conv2DTranspose(512, kernel_size=4, strides=2, padding="same"),
-        layers.LeakyReLU(alpha=0.2),
-        layers.Conv2D(3, kernel_size=5, padding="same", activation="sigmoid"),
+        layers.Conv2DTranspose(1, kernel_size=4, strides=2, padding="same", activation="sigmoid"),
     ],
     name="generator",
 )
@@ -137,32 +133,70 @@ class GANMonitor(keras.callbacks.Callback):
             img = keras.utils.array_to_img(generated_images[i])
             img.save("generated_img_%03d_%d.png" % (epoch, i))
 
+def plot_latent_space(gan, n=30, figsize=15):
+    # display an n*n 2D manifold of digits
+    digit_size = 28
+    scale = 1.0
+    figure = np.zeros((digit_size * n, digit_size * n))
+    # linearly spaced coordinates corresponding to the 2D plot
+    # of digit classes in the latent space
+    grid_x = np.linspace(-scale, scale, n)
+    grid_y = np.linspace(-scale, scale, n)[::-1]
+
+    for i, yi in enumerate(grid_y):
+        for j, xi in enumerate(grid_x):
+            z_sample = np.array([[xi, yi]])
+            x_decoded = gan.generator(z_sample)
+            digit = tf.reshape(x_decoded[0], (digit_size, digit_size))
+            figure[
+            i * digit_size : (i + 1) * digit_size,
+            j * digit_size : (j + 1) * digit_size,
+            ] = digit
+
+    plt.figure(figsize=(figsize, figsize))
+    start_range = digit_size // 2
+    end_range = n * digit_size + start_range
+    pixel_range = np.arange(start_range, end_range, digit_size)
+    sample_range_x = np.round(grid_x, 1)
+    sample_range_y = np.round(grid_y, 1)
+    plt.xticks(pixel_range, sample_range_x)
+    plt.yticks(pixel_range, sample_range_y)
+    plt.xlabel("z[0]")
+    plt.ylabel("z[1]")
+    plt.imshow(figure, cmap="Greys_r")
+    plt.show()
+
 
 if __name__ == "__main__":
+    import numpy as np
     # データセット用意
-    dataset_dir = "celeba_gan"
+    # dataset_dir = "celeba_gan"
+    #
+    # if not os.path.exists(dataset_dir):
+    #     os.makedirs(dataset_dir)
+    #
+    # url = "https://drive.google.com/uc?id=1O7m1010EJjLE5QxLZiM9Fpjs7Oj6e684"
+    # output = "celeba_gan/data.zip"
+    #
+    # if not os.path.exists(output):
+    #     gdown.download(url, output, quiet=True)
+    #
+    #     with ZipFile("celeba_gan/data.zip", "r") as z:
+    #         z.extractall("celeba_gan")
+    #
+    # dataset = keras.utils.image_dataset_from_directory(
+    #     "celeba_gan", label_mode=None, image_size=(64, 64), batch_size=BATCH_SIZE
+    # )
+    # dataset = dataset.map(lambda x: x / 255.0)
+    #
+    # for x in dataset:
+    #     plt.axis("off")
+    #     plt.imshow((x.numpy() * 255).astype("int32")[0])
+    #     break
 
-    if not os.path.exists(dataset_dir):
-        os.makedirs(dataset_dir)
-
-    url = "https://drive.google.com/uc?id=1O7m1010EJjLE5QxLZiM9Fpjs7Oj6e684"
-    output = "celeba_gan/data.zip"
-
-    if not os.path.exists(output):
-        gdown.download(url, output, quiet=True)
-
-        with ZipFile("celeba_gan/data.zip", "r") as z:
-            z.extractall("celeba_gan")
-
-    dataset = keras.utils.image_dataset_from_directory(
-        "celeba_gan", label_mode=None, image_size=(64, 64), batch_size=BATCH_SIZE
-    )
-    dataset = dataset.map(lambda x: x / 255.0)
-
-    for x in dataset:
-        plt.axis("off")
-        plt.imshow((x.numpy() * 255).astype("int32")[0])
-        break
+    (x_train, _), (x_test, _) = keras.datasets.mnist.load_data()
+    mnist_digits = np.concatenate([x_train, x_test], axis=0)
+    mnist_digits = np.expand_dims(mnist_digits, -1).astype("float32") / 255
 
     # train GAN
     gan = GAN(discriminator=discriminator, generator=generator, latent_dim=LATENT_DIM)
@@ -173,7 +207,10 @@ if __name__ == "__main__":
     )
 
     gan.fit(
-        dataset,
+        mnist_digits,
         epochs=EPOCHS,
         callbacks=[GANMonitor(num_img=10, latent_dim=LATENT_DIM)],
     )
+
+    # 潜在空間のビジュアライズ
+    # plot_latent_space(gan)
