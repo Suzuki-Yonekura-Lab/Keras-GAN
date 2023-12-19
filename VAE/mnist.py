eba6751c4f540935d9d816e1d9d9e612535a8400
geeeeorge <yuto.george@i.softbank.jp>
2023-12-20 02:55:03 +0900

    feat: impl VAEMonitor

--- VAE/mnist.py
+++ VAE/mnist.py
@@ -90,6 +90,21 @@ class VAE(keras.Model):
         }
 
 
+class VAEMonitor(keras.callbacks.Callback):
+    def __init__(self, num_img=3, latent_dim=2):
+        self.num_img = num_img
+        self.latent_dim = latent_dim
+
+    def on_epoch_end(self, epoch, logs=None):
+        random_latent_vectors = tf.random.normal(shape=(self.num_img, self.latent_dim))
+        generated_images = self.model.decoder(random_latent_vectors)
+        generated_images *= 255
+        generated_images.numpy()
+        for i in range(self.num_img):
+            img = keras.utils.array_to_img(generated_images[i])
+            img.save("VAE/output/generated_img_%03d_%d.png" % (epoch, i))
+
+
 def plot_latent_space(vae, n=30, figsize=15):
     # display an n*n 2D manifold of digits
     digit_size = 28
@@ -143,7 +158,12 @@ if __name__ == "__main__":
 
     vae = VAE(encoder, decoder)
     vae.compile(optimizer=keras.optimizers.Adam())
-    vae.fit(mnist_digits, epochs=EPOCHS, batch_size=BATCH_SIZE)
+    vae.fit(
+        mnist_digits,
+        epochs=EPOCHS,
+        batch_size=BATCH_SIZE,
+        callbacks=[VAEMonitor(num_img=10, latent_dim=LATENT_DIM)],
+    )
 
     # 潜在空間のビジュアライズ
     plot_latent_space(vae)
